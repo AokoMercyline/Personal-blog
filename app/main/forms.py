8aec896b30bd58d8373a17bb15dfecdea6db2ee8
AokoMercyline <aokomercyline34@gmail.com>
2020-09-28 22:10:41 +0300

    created class for post and comment

--- app/main/forms.py
+++ app/main/forms.py
@@ -5,3 +5,12 @@ from wtforms.validators import Required
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.',validators = [Required()])
     submit = SubmitField('Submit')
+    
+class PostForm(FlaskForm):
+    post_title = StringField('Title')
+    description = TextAreaField('Description', validators=[Required()])
+    submit = SubmitField('Submit')
+    
+class CommentForm(FlaskForm):
+    comment = TextAreaField('write a comment',validators=[Required()])
+    submit = SubmitField('comment')
